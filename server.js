28e99c13986ee1e999f6702fb547533df5e5a1e5
Cameron Buscher <cameronbuscher@gmail.com>
2018-07-16 09:58:38 -0600

    Add get endpoint for all items

--- server.js
+++ server.js
@@ -14,3 +14,13 @@ app.use(express.static('public'));
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}`)
 })
+
+app.get('/api/v1/items', (request, response) => {
+  database('items').select()
+    .then(items => {
+      response.status(200).json(items)
+    })
+    .catch(error => {
+      response.status(500).json({error})
+    })
+})
