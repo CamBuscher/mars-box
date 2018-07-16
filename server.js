1177188d39a8e6df622e2ea5b7cf2aa1fa3040a2
Cameron Buscher <cameronbuscher@gmail.com>
2018-07-16 09:45:39 -0600

    Setup server file

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Mars Box';
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}`)
+})
